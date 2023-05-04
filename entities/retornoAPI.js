e942cf7435a3c6aa5827e7e95ddb4bef1519864c
LUCAS LOSANO NASCIMENTO <losano3@hotmail.com>
2023-05-04 19:58:14 -0300

    Removendo arquivos

--- entities/retornoAPI.js
+++
@@ -1,9 +0,0 @@
-class retornoAPI{
-    constructor(retornoAPI){
-        this.sucesso = retornoAPI.sucesso;
-        this.retorno = retornoAPI.retorno;
-        this.erro = retornoAPI.erro;
-    }
-}
-
-module.exports = retornoAPI;
