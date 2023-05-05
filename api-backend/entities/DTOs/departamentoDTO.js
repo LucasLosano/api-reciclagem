64801cffbe7fefc503685796a0bf7499348f32be
LucasLosano <losano3@hotmail.com>
2023-05-05 16:08:14 -0300

    Delete api-backend directory

--- api-backend/entities/DTOs/departamentoDTO.js
+++
@@ -1,9 +0,0 @@
-class departamentoDTO{
-    constructor(departamento){
-        this.id = departamento.id;
-        this.nome = departamento.nome;
-        this.pesagens = departamento.pesagens;
-    }
-}
-
-module.exports = departamentoDTO;
--- api-backend/entities/DTOs/recompensaDTO.js
+++
@@ -1,9 +0,0 @@
-class recompensaDTO{
-    constructor(recompensa){
-        this.id = recompensa.id;
-        this.nome = recompensa.nome;
-        this.pontosNecessarios = recompensa.pontosNecessarios;
-    }
-}
-
-module.exports = recompensaDTO;
