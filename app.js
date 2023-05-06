9207850169ec84bffaf1c76274cb7288dfded784
LucasLosano <losano3@hotmail.com>
2023-05-05 21:49:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var bodyParser = require('body-parser');
 
 var departamentos = require('./routes/departamentos');
 var pesagens = require ('./routes/pesagens');
-
 var recompensas = require('./routes/recompensas');
 
 var app = express();
@@ -16,7 +15,8 @@ app.use(cookieParser())
 
 app.use('/api/v1/departamentos', departamentos);
 app.use('/api/v1/pesagens', pesagens);
-
 app.use('/api/v1/recompensas', recompensas);
+console.log(process.env.AZURE_MONGODB)
+console.log(process.env.AZURE_DATABASE)
 
 module.exports = app;
