a3c1a798854b17e919401e54cb7a9985aecdab4e
Lucas Losano <losano3@hotmail.com>
2023-05-07 04:14:02 -0300

    Departamentos tela e roteamento

---
+++ front-end/src/app/departamento/departamento-list/departamento-list.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { DepartamentoService } from 'src/app/services/departamento.service';
+import { DepartamentoModel } from 'src/app/models/departamentoModel';
+
+@Component({
+  selector: 'app-departamento-list',
+  templateUrl: './departamento-list.component.html',
+  styleUrls: ['./departamento-list.component.css']
+})
+export class DepartamentoListComponent {
+  departamentos: DepartamentoModel[] = [];
+  errorMessage: string = "";
+  formFlag: boolean = false;
+  departamentoSelect?: DepartamentoModel;
+
+  constructor(
+    private departamentoService: DepartamentoService
+  ) {
+  }
+
+  cancelar(){
+    this.formFlag = false;
+    this.departamentoSelect = undefined;
+  }
+
+  edit(departamento: DepartamentoModel){
+    this.departamentoSelect = departamento;
+    this.formFlag = true;
+  }
+
+  delete(id: number){
+    this.departamentoService.deleteById(id)
+      .subscribe(data => {        
+        if(!data.sucesso)
+          this.errorMessage = data.error;
+      })
+  }
+
+  ngOnInit() {
+    this.departamentoService.getAll()
+      .subscribe(data => {        
+        if(data.sucesso)
+          this.departamentos = data.retorno;
+        else
+          this.errorMessage = data.error;
+      })
+  }
+}
