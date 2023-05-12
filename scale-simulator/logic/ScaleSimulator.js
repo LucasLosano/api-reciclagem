067f05b0f775d958ba133623fd786769ce83f6ae
arthur-maloy <maloyama.arthur@gmail.com>
2023-05-12 00:38:06 -0300

    Adição Token

--- scale-simulator/logic/ScaleSimulator.js
+++ scale-simulator/logic/ScaleSimulator.js
@@ -4,7 +4,7 @@ function validarFormulario() {
     const departamento = document.getElementById('departamento').value;
     const link = document.getElementById('link').value;
     const identificador = document.getElementById('identificador').value;
-
+    
     if (!peso || !material || !departamento || !link || !identificador) {
         alert('Por favor, preencha todos os campos obrigatórios!');
     } else {
@@ -12,7 +12,7 @@ function validarFormulario() {
         console.log(material);
         console.log(departamento);
         console.log(link);
-
+        
         enviarFormulario(peso, material, departamento, link, identificador);
     }
 }
@@ -28,21 +28,23 @@ async function enviarFormulario(peso, material, departamento, link, identificado
         nome: material,
         id: identificador
     });
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDVkYWM3NmE0M2I2NzQ3ODc5NTgyOTciLCJpYXQiOjE2ODM4NjA2MDN9.nmKIVjLWxA0cWdZkiNuml-4FNQnCMPUtpJkn6Z6tka8';
     const response = await fetch(link, {
         method: 'POST',
         mode: 'cors',
         headers: {
+            'Authorization': 'Bearer ' + token,
             'Content-Type': 'application/json'
         },
         body: data
     });
-
+    
     if (response.ok) {
-        alert('Pedido enviado com sucesso!');
+        alert('Requisição enviado com sucesso!');
         console.log(await response.text());
         clearFields();
     } else {
-        alert('Houve um erro ao enviar o pedido.');
+        alert('Houve um erro ao enviar a Requisição.');
     }
 }
 
