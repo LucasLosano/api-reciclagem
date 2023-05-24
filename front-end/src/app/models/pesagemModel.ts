c5ec48542826945f864270427c08a3c71a04e8d5
Lucas Losano <losano3@hotmail.com>
2023-05-23 23:01:21 -0300

    Fixe

--- front-end/src/app/models/pesagemModel.ts
+++ front-end/src/app/models/pesagemModel.ts
@@ -1,10 +1,5 @@
 export interface PesagemModel{
     id: number;
     peso: number;
-<<<<<<< Updated upstream
-    tipoMaterial: number;
-}
-=======
     materialId: number;
 }
->>>>>>> Stashed changes
